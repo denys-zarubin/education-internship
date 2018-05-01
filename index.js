a74531fccf2f34c27927ab2ba8799608c8b65d91
pnesterenko <pnesterenko@connecture.com>
2018-05-01 15:50:20 +0300

    commit from sashaVerstka

---
+++ index.js
@@ -0,0 +1,15 @@
+const menuBarNav = document.getElementsByClassName("navigation-bar")[0]
+const subMenu = document.getElementsByClassName("sub-navigation-list")[0]
+
+
+
+
+function changeView({target}){
+    target.style.display = "none"
+    subMenu.style.display = "block"
+    console.log(target)
+}
+
+
+menuBarNav.addEventListener("click", changeView)
+
